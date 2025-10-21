e9087618c13edcb3007b6add966de45fd306c8e0
marceloayubbueno <marcelo_ayub@hotmail.com>
2025-10-21 20:15:41 -0300

    fix: Excluir pasta server do build do Next.js na Vercel

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
       'upload.wikimedia.org',
     ],
   },
+  // Excluir pasta server do build do Next.js
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/server/**', '**/node_modules/**'],
+    }
+    return config
+  },
+  // Excluir arquivos do backend
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  exclude: ['server/**/*'],
 }
 
 module.exports = nextConfig 
